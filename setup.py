edf26dfdb24320b5734c7507ddcf62cc4f6a387b
shivakumarsanugula <shivakumaryadav1997@gmail.com>
2024-01-06 15:40:17 +0530

    code updated

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+REPO_NAME = "MongoDB_Connector"
+PKG_NAME= "MongoDB_Connector_PKG"
+AUTHOR_USER_NAME = "shivakumarsanugula"
+AUTHOR_EMAIL = "shivakumaryadav1997@gmail.com"
 
 setup(
     name=PKG_NAME,
